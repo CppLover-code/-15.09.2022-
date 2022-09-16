0282b53ce20694261e7c7e048f5318a16562d60e
Hp <annaboiko377@gmail.com>
2022-09-17 01:27:00 +0300

    !!

--- ДЗ 15.09.2022 Указатель на функцию/ДЗ 15.09.2022 Указатель на функцию.cpp
+++ ДЗ 15.09.2022 Указатель на функцию/ДЗ 15.09.2022 Указатель на функцию.cpp
@@ -56,15 +56,143 @@
 //    return 0;
 //}
 
+//// Задание 2. 
+//// Первый вариант
+//
+//#include <iostream>
+//using namespace std;
+//
+//void func(int* ar1, int* ar2, int* ar3, size_t k, size_t v)
+//{
+//    int sum1 = 0, sum2 = 0;;
+//
+//    for (size_t i = 0; i < v; )
+//    {
+//        for (size_t x = 0; x < k; x++)
+//        {
+//            sum1 += ar1[x];
+//        }
+//        //cout << " A " << sum1 << "\n";
+//        ar3[i] = sum1;
+//        i++;
+//
+//        for (size_t y = 0; y < k; y++)
+//        {
+//            sum2 += ar2[y];
+//        }
+//        //cout << " B " << sum2 << "\n";
+//        ar3[i] = sum2;
+//        i++;
+//    }
+//    cout << "MasC ";
+//    for (size_t i = 0; i < v; i++)
+//    {
+//        cout << ar3[i] << " ";
+//    }
+//
+//}
+//
+//int main()
+//{
+//    size_t k = 7;
+//    size_t v = 2;
+//    int* A = new int[k] { 5, 5, 5, 5, 5, 5, 5 };
+//    int* B = new int[k] { 6,6, 6, 6, 6, 6, 6 };
+//    int* C = new int[v];
+//
+//    func(A, B, C, k, v);
+//
+//    delete[] A;
+//    delete[] B;
+//    delete[] C;
+//
+//    return 0;
+//}
+
 // Задание 2. 
+// Второй вариант
 
 #include <iostream>
 using namespace std;
 
+typedef int(*oper_t) (int*, size_t);
+
+//void func(int* ar1, int* ar2, int* ar3, size_t k, size_t v)
+//{
+//    int sum1 = 0, sum2 = 0;;
+//
+//    for (size_t i = 0; i < v; )
+//    {
+//        for (size_t x = 0; x < k; x++)
+//        {
+//            sum1 += ar1[x];
+//        }
+//        //cout << " A " << sum1 << "\n";
+//        ar3[i] = sum1;
+//        i++;
+//
+//        for (size_t y = 0; y < k; y++)
+//        {
+//            sum2 += ar2[y];
+//        }
+//        //cout << " B " << sum2 << "\n";
+//        ar3[i] = sum2;
+//        i++;
+//    }
+//    cout << "MasC ";
+//    for (size_t i = 0; i < v; i++)
+//    {
+//        cout << ar3[i] << " ";
+//    }
+//
+//}
+
+int sumA(int* ar1, size_t k)
+{
+    int sum1 = 0;
+    for (size_t x = 0; x < k; x++)
+    {
+        sum1 += ar1[x];
+    }
+    return sum1;
+}
+int sumB(int* ar2, size_t k)
+{
+    int sum2 = 0;
+    for (size_t x = 0; x < k; x++)
+    {
+        sum2 += ar2[x];
+    }
+    return sum2;
+}
+
+
 
 int main()
 {
-   
+    size_t k = 7;
+    
+    int* A = new int[k] { 5, 5, 5, 5, 5, 5, 5 };
+    int* B = new int[k] { 6, 6, 6, 6, 6, 6, 6 };
+    //int* C = new int[v];
+    size_t v = 2;
+    oper_t* C = new oper_t[v]{ sumA, sumB };
+
+    //func(A, B, C, k, v);
+    for (size_t i = 0; i < v; )
+    {
+        cout << C[i](A, k) << "\n";
+        i++;
+        cout << C[i](B, k) << "\n";
+        i++;
+    }
+    
+
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
 
     return 0;
 }
+
